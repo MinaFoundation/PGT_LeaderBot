0489c890f43c3628208f5e992d9ed4d756a9ac54
berkingurcan <berkingurcan@yandex.com>
2024-07-13 21:30:41 +0300

    feat add token calculator and tests

--- github_tracker_bot/helpers/calculate_token.py
+++ github_tracker_bot/helpers/calculate_token.py
@@ -2,3 +2,15 @@
 # Return if exceeds
 # if exceeds use divider(diff_divider.py)
 # use it before ``message = prompts.process_message(date, data_array)`` and give separate arrays according to exceed
+import tiktoken
+import github_tracker_bot.prompts as prompts
+
+def calculate_token_number(data):
+    system_token_count = prompts.SYSTEM_MESSAGE_DAILY_DECIDE_COMMIT
+    message_token_count = 1000
+
+    enc = tiktoken.encoding_for_model("gpt-4o")
+    token_integers = enc.encode(system_token_count + " " + data)
+    num_token = len(token_integers) + message_token_count
+
+    return num_token < 120000
