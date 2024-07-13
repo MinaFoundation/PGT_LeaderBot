ec4b258229e092ea991a9fd0eeeeff8d5c8b8a08
berkingurcan <berkingurcan@yandex.com>
2024-07-13 20:33:31 +0300

    style add todos

--- github_tracker_bot/helpers/calculate_token.py
+++ github_tracker_bot/helpers/calculate_token.py
@@ -1,3 +1,4 @@
 # Calculate token with Prompts and diffs 
 # Return if exceeds
-# if exceeds use divider(create new helper script)
+# if exceeds use divider(diff_divider.py)
+# use it before ``message = prompts.process_message(date, data_array)`` and give separate arrays according to exceed
