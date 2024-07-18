eed4d3636cc50215cb06d9c8ad26c49bed2a541d
berkingurcan <berkingurcan@yandex.com>
2024-07-18 12:41:50 +0300

    style add better info to excessive data

--- github_tracker_bot/helpers/handle_daily_commits_exceed_data.py
+++ github_tracker_bot/helpers/handle_daily_commits_exceed_data.py
@@ -8,7 +8,7 @@ def handle_daily_exceed_data(daily_commit_data):
     if not token_count:
         for commit_data in daily_commit_data:
             commit_data["diff"] = (
-                "The diff file exceeds OPENAI token limit. The diff data possibly includes spam data."
+               "The diff file exceeds the OPENAI token limit. The diff data possibly includes meaningless excessively large data."
             )
     else:
         return daily_commit_data
