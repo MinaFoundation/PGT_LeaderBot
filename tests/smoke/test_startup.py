14c343175b4c3729599e573a1c928b8deb860de3
berkingurcan <berkingurcan@yandex.com>
2024-10-21 14:18:49 +0700

    feat add test health check

--- tests/smoke/test_startup.py
+++ tests/smoke/test_startup.py
@@ -6,7 +6,7 @@ from httpx import AsyncClient, ASGITransport
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
-    
+
 import config
 
 from github_tracker_bot.bot import app
@@ -20,3 +20,14 @@ async def test_app_startup():
     async with AsyncClient(transport=transport, base_url="http://test") as client:
         response = await client.get("/non-existing-endpoint", headers=headers)
         assert response.status_code == 404
+
+
+@pytest.mark.smoke
+@pytest.mark.asyncio
+async def test_app_health_check():
+    headers = {"Authorization": config.SHARED_SECRET}
+
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.get("/health", headers=headers)
+        assert response.status_code == 200
+        assert response.json() == {"status": "OK"}
